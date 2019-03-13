d71940336e3a164a924789b051a46a176a1e6d62
JINSAN NOH <zingoworks@gmail.com>
2019-03-13 14:24:20 +0900

    숫자야구

---
+++ src/main/java/programmers/lv2/숫자야구.java
@@ -0,0 +1,57 @@
+package programmers.lv2;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class 숫자야구 {
+    //Doby 코드 보고 리팩토링만...!
+
+    public int solution(int[][] baseball) {
+        int count = 0;
+
+        for (int i = 123; i <= 987; i++) {
+            String target = String.valueOf(i);
+            outer:
+            if(!target.contains("0")
+                    && target.charAt(0) != target.charAt(1)
+                    && target.charAt(1) != target.charAt(2)
+                    && target.charAt(0) != target.charAt(2)) {
+                for (int[] ints : baseball) {
+                    String one = String.valueOf(ints[0]);
+                    int strikes = countStrikes(one, target);
+                    int balls = countBalls(one, target, strikes);
+
+                    if (strikes != ints[1] || balls != ints[2]) {
+                        break outer;
+                    }
+                }
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private int countStrikes(String one, String target) {
+        int count = 0;
+        for (int i = 0; i < target.length(); i++) {
+            count = one.charAt(i) == target.charAt(i) ? count + 1 : count;
+        }
+        return count;
+    }
+
+    private int countBalls(String one, String target, int strikes) {
+        int count = 0;
+        for (int i = 0; i < target.length(); i++) {
+            count = one.contains(String.valueOf(target.charAt(i))) ? count + 1 : count;
+        }
+        return count - strikes;
+    }
+
+    @Test
+    public void test() {
+        int[][] baseball = {{123, 1, 1}, {356, 1, 0}, {327, 2, 0}, {489, 0, 1}};
+        assertThat(solution(baseball)).isEqualTo(2);
+    }
+}
