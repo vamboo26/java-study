1be50aee1caf112b15dcfa6592f7c092e8c7268e
JINSAN NOH <zingoworks@gmail.com>
2019-03-20 11:43:52 +0900

    Exception test

---
+++ src/main/java/etc/ExceptionTest.java
@@ -0,0 +1,23 @@
+package etc;
+
+public class ExceptionTest {
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+
+    public static String hello() {
+        System.out.println("123");
+
+        try {
+            System.out.println("try문 시작");
+            return "try hello";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally문 시작");
+        }
+
+        return "finally hello";
+
+    }
+}
