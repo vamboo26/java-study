b34036675ec2fe6a239b875d1630db80d5d0fb00
JINSAN NOH <zingoworks@gmail.com>
2019-03-28 19:17:50 +0900

    Add other solution

--- src/main/java/programmers/lv2/타겟넘버.java
+++ src/main/java/programmers/lv2/타겟넘버.java
@@ -1,5 +1,11 @@
 package programmers.lv2;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 public class 타겟넘버 {
     private static int answer = 0;
 
@@ -29,5 +35,27 @@ public class 타겟넘버 {
 
         numbers[depth] *= -1;
         findTarget(numbers, target, depth + 1);
+    }
+
+    //TODO : Googled solution
+    //전역변수 쓰지 않으며, int 반환
+    public int 참고풀이(int[] numbers, int target) {
+        return dfs(numbers, 0, 0, target);
+    }
+
+    private int dfs(int[] numbers, int depth, int sum, int target) {
+        if(depth == numbers.length) {
+            return (sum == target) ? 1 : 0;
+        }
+
+        return dfs(numbers, depth + 1, sum + numbers[depth], target) + dfs(numbers, depth + 1, sum - numbers[depth], target);
+    }
+
+    @Test
+    public void test() {
+        int[] numbers = new int[5];
+        Arrays.fill(numbers, 1);
+
+        assertThat(참고풀이(numbers, 3)).isEqualTo(5);
     }
 }
