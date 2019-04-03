ffd4ad08a2ba882b3f1c431ed5993f4c7332d39c
JINSAN NOH <zingoworks@gmail.com>
2019-04-03 19:51:49 +0900

    수 정렬하기

---
+++ src/main/java/boj/Q2750.java
@@ -0,0 +1,13 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//수 정렬하기
+public class Q2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
