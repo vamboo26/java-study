18061cd55258ab55afb576b21312ceacffd54a6d
JINSAN NOH <zingoworks@gmail.com>
2019-04-09 12:01:37 +0900

    Q4811

---
+++ src/main/java/boj/recursion/Q4811.java
@@ -0,0 +1,50 @@
+package boj.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+//알약
+//TODO : Googled solution
+public class Q4811 {
+    private static long[][] memorize;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n;
+
+        while ((n = Integer.valueOf(br.readLine())) != 0) {
+            memorize = new long[2 * n + 1][2 * n + 1];
+            int w = n - 1;
+            int h = 1;
+            System.out.println(split(w, h));
+        }
+    }
+
+    private static long split(int w, int h) {
+        if (w > 0) {
+            if (memorize[w][h] != 0) {
+                return memorize[w][h];
+            }
+        }
+
+        if (w == 0) {
+            memorize[w][h] = 1;
+            return 1;
+        }
+
+        long sum = 0;
+        sum += split(w - 1, h + 1);
+
+        if(h >= 1) {
+            sum += split(w, h - 1);
+        }
+
+        memorize[w][h] = sum;
+        return sum;
+    }
+}
