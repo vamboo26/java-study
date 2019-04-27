a2115c9e79862f1edff5e97c2be22d410b813ad7
JINSAN NOH <zingoworks@gmail.com>
2019-04-27 15:41:23 +0900

    1300

---
+++ src/main/java/boj/Q1300.java
@@ -0,0 +1,34 @@
+package boj;
+
+import java.io.*;
+
+//K번째수
+//TODO : Googled solution
+public class Q1300 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        int low = 1, high = k;
+        int result = -1;
+
+        while (low <= high) {
+            int count = 0;
+            int mid = (low + high) / 2;
+            for (int i = 1; i <= n; i++)
+                count += Math.min(mid / i, n);
+            if (count < k)
+                low = mid + 1;
+            else {
+                result = mid;
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+}
