a64d6f4807ff6670a0a6406c9bf1390de26694b5
zingoworks <zingoworks@gmail.com>
2019-05-15 04:45:13 +0900

    Array Manipulation

---
+++ src/main/java/hackerrank/array/Array_Manipulation.java
@@ -0,0 +1,65 @@
+package hackerrank.array;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Array_Manipulation {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+        long[] result = new long[n + 2];
+
+        for (int[] query : queries) {
+            int k = query[2];
+            result[query[0]] += k;
+            result[query[1] + 1] -= k;
+        }
+
+        return getMax(result);
+    }
+
+    static long getMax(long[] input) {
+        long max = Long.MIN_VALUE;
+        long sum = 0;
+        for (long each : input) {
+            sum += each;
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
