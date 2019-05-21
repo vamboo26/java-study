770ad044dfc767eb5f0a991df6294d54a7711fa6
zingoworks <zingoworks@gmail.com>
2019-05-22 01:57:14 +0900

    k tests

---
+++ src/main/java/etc/KTEST_1.java
@@ -0,0 +1,30 @@
+package etc;
+
+import org.junit.Test;
+
+public class KTEST_1 {
+    //16분
+
+    public int solution(int[] v) {
+        int sum = 0;
+        int combo = 0;
+
+        for (int i = 0; i < v.length; i++) {
+            if(v[i] != 0) {
+                combo++;
+                sum += (i + 1) * combo;
+            } else {
+                combo = 0;
+            }
+        }
+
+        return sum;
+    }
+
+    @Test
+    public void test() {
+        int[] v = {0,1,1,0,1,1,1,0};
+        System.out.println(solution(v));
+    }
+
+}
