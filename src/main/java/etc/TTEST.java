76c1d316816910b87259b4278d02d78b6e884f7c
zingoworks <zingoworks@gmail.com>
2019-05-24 05:06:44 +0900

    Ttest

---
+++ src/main/java/etc/TTEST.java
@@ -0,0 +1,14 @@
+package etc;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+//3시간 1문제
+public class TTEST {
+
+}
