7aae1cf6421b5a707d8d1690b7055333b6ae2cf4
JINSAN NOH <zingoworks@gmail.com>
2019-08-11 14:19:03 +0900

    update

--- src/main/java/programmers/lv2/큰수만들기.java
+++ src/main/java/programmers/lv2/큰수만들기.java
@@ -1,5 +1,7 @@
 package programmers.lv2;
 
+import java.util.Stack;
+
 public class 큰수만들기 {
 
     public String solution(String number, int k) {
@@ -18,5 +20,27 @@ public class 큰수만들기 {
 
         return sb.toString();
     }
+
+    public String solution2(String number, int k) {
+        int targetLength = number.length() - k;
+
+        Stack<Character> letters = new Stack<>();
+        for(char c : number.toCharArray()) {
+            while(!letters.isEmpty() && k-- > 0 && letters.peek() < c) {
+                letters.pop();
+            }
+
+            letters.push(c);
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < targetLength; i++) {
+            answer.append(letters.get(i));
+        }
+
+        return answer.toString();
+    }
+
+    //StringBuilder 대신 char 배열 길이만큼 선언 후 new String(char배열)도 가능
 
 }
